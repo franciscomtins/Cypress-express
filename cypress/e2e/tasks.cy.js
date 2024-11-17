1c5bde1a2c2794d03704248bf4eea65a898f80f9
Francisco Martins <francisco.mtins@gmail.com>
2024-11-17 17:30:10 -0300

    Alterando estratégia da massa de testes e validando cadastro

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -4,6 +4,16 @@
 describe('tarefas', () =>{
 
     it.only('deve cadastrar uma nova tarefa', ()=> {
+
+        cy.request({
+            url: 'http://localhost:3333/helper/tasks',
+            method: 'DELETE',
+            body: {name: 'Ler um livro de Node.js'}
+        }).then(response => {
+            expect(response.status).to.equal(204)
+        })
+
+
         cy.visit('http://localhost:3000/')
 
         cy.get('input[placeholder="Add a new Task"]')
@@ -11,6 +21,13 @@ describe('tarefas', () =>{
             .type('Ler um livro de Node.js')
         
         cy.contains('button', 'Create').click()
+
+        // cy.get('main div p')
+        //     .should('be.visible')
+        //     .should('have.text', 'Ler um livro de Node.js')
+
+        cy.contains('main div p', 'Ler um livro de Node.js')
+            .should('be.visible')
 
     })
 
