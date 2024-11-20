0c3ca66b7b76756e7845e4aac01ef7fb4088d2cb
Francisco Martins <francisco.mtins@gmail.com>
2024-11-20 10:58:37 -0300

    Integração com Allure Reports

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,18 @@
 const { defineConfig } = require("cypress");
 const webpack = require('@cypress/webpack-preprocessor');
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
     env: {
       apiUrl: 'http://localhost:3333'
-    } ,
+    },
+    viewportWidth: 1920,
+    viewportHeight: 1080,
     setupNodeEvents(on, config) {
+      allureWriter(on, config);
+      return config;
       // Configura o webpack como preprocessor
       const options = {
         webpackOptions: require('./webpack.config.js'), // Inclua o caminho correto para o webpack.config.js
--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -28,7 +28,7 @@ describe('tarefas', () =>{
                 .should('be.visible')
         })
     
-        it.only('não deve permitir tarefa duplicada', ()=> {
+        it('não deve permitir tarefa duplicada', ()=> {
     
             const task =  testData.dup
                 
